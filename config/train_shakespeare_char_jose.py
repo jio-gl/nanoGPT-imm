1cfc011e3afc08bb2c2343e5048d19dfce0dbad7
José <30682875+jo-tm@users.noreply.github.com>
2025-04-16 01:25:51 +0200

    separate train_imm.py to import and sep config

--- config/train_shakespeare_char_jose.py
+++ config/train_shakespeare_char_jose.py
@@ -15,13 +15,13 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 32 # reduced from 64 for faster training
+block_size = 128 # reduced from 256 for faster training
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 4 # reduced from 6
+n_head = 4 # reduced from 6
+n_embd = 256 # reduced from 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
@@ -30,7 +30,7 @@ lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 10 # not super necessary potentially
 
 # on macbook also add
 device = 'cpu'  # run on cpu only
